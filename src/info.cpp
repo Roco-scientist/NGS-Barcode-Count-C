0428c4df8aa86f6b13073ded80ded8dd7354b679
Rory Coffey <coffeyrt@gmail.com>
2021-10-27 20:09:45 -0400

    documentation added

--- src/info.cpp
+++ src/info.cpp
@@ -2,27 +2,45 @@
 
 using namespace std;
 
+/**
+ * Funtion to pull the first <num_colums> data from each pass comma separated
+ * string
+ */
 vector<string> take_columns(int num_columns, string row) {
-	int pos;
-	string token;
-	vector<string> data_points;
-	int current_column = 1;
+	int pos;		     // keep track of the column position
+	string data;		     // column,row data holder
+	vector<string> data_points;  // vector to contain all data points
+	int current_column = 1;	     // start at column 1
+	// iterate through each comma point and pull out the data.  Stop when
+	// there are no more commas or the num_columns is reached
 	while ((pos = row.find(',')) != string::npos &&
 	       current_column <= num_columns) {
-		token = row.substr(0, pos);
-		data_points.push_back(token);
-		row.erase(0, pos + 1);
+		data = row.substr(0, pos);
+		data_points.push_back(data);
+		row.erase(
+		    0,
+		    pos +
+			1);  // remove the data from the string up to the comma
 		current_column++;
 	}
+	// If the the last column was the num_columns then push the last string
+	// data of the row
 	if (current_column == num_columns) {
 		data_points.push_back(row);
-	} else if (current_column < num_columns) {
+	} else if (current_column < num_columns) {  // If there are not enough
+						    // columns, exit with error
 		cout << "Not enough columns" << endl;
 		exit(1);
 	}
 	return data_points;
 }
 
+/**
+ * Pulls in the given sample barcode file and pushes the data into an
+ * unodered_map. This map is later used to convert DNA barcode to sample name.
+ * Additionally, an unordered_set of sequences is created to error correct for
+ * any sequencing errors.
+ */
 void BarcodeConversion::sample_barcode_conversion(const string barcode_path) {
 	ifstream barcode_file;
 	barcode_file.open(barcode_path);
@@ -37,10 +55,18 @@ void BarcodeConversion::sample_barcode_conversion(const string barcode_path) {
 		vector<string> row_vec = take_columns(2, row);
 		samples_barcode_hash.insert(
 		    std::pair<string, string>(row_vec[0], row_vec[1]));
+		samples_seqs.insert(row_vec[0]);
 	}
 }
 
+/**
+ * Pulls in the given counted barcodes file and pushes the data into an
+ * unodered_map for each sequential barcode. These maps are later used to
+ * convert DNA barcode to sample name. Additionally, unordered_sets of the
+ * sequences are created to error correct for any sequencing errors.
+ */
 void BarcodeConversion::barcode_file_conversion(const string barcode_path) {
+	// Open the file and handle not found errors
 	ifstream barcode_file;
 	barcode_file.open(barcode_path);
 	if (!barcode_file.is_open()) {
@@ -48,17 +74,22 @@ void BarcodeConversion::barcode_file_conversion(const string barcode_path) {
 		exit(1);
 	}
 
+	// keep track of number of barcodes, used later
 	unordered_set<int> barcode_num;
+	// hold all comma separated data to insert into hashmaps
 	vector<vector<string>> row_info;
 
 	string row;
 	std::getline(barcode_file, row);  // The first row should be the header
+	// Collect all comma separated rows and keep track of how many barcodes
 	for (string row; std::getline(barcode_file, row);) {
 		vector<string> row_vec = take_columns(3, row);
 		row_info.push_back(row_vec);
 		barcode_num.insert(std::stoi(row_vec[2]));
 	}
 
+	// Get total number of counted barcodes per sequence. ie for DEL this
+	// can be 3 or 4
 	int total_barcodes;
 	for (const auto &num : barcode_num) {
 		if (num > total_barcodes) {
@@ -66,19 +97,27 @@ void BarcodeConversion::barcode_file_conversion(const string barcode_path) {
 		}
 	}
 
-	unordered_map<string, string> empty_hashmap;
+	// For each counted barcode in a sequence, create empty hashmaps and
+	// hashsets
+	stringmap empty_hashmap;
+	stringset empty_hashset;
 	for (int i = 0; i < total_barcodes; i++) {
 		counted_barcodes_hash.push_back(empty_hashmap);
+		counted_barcodes_seqs.push_back(empty_hashset);
 	}
 
+	// Insert info into vectors of hashmaps and hashsets while using the
+	// index as the counted barcode number
 	for (int i = 0; i < row_info.size(); i++) {
 		vector<string> row_vec = row_info[i];
 		int barcode_num = std::stoi(row_vec[2]) - 1;
 		pair<string, string> barcode_conv{row_vec[0], row_vec[1]};
 		counted_barcodes_hash[barcode_num].insert(barcode_conv);
+		counted_barcodes_seqs[barcode_num].insert(row_vec[0]);
 	}
 }
 
+/// A print method created to test building of the algorithm
 void BarcodeConversion::print() {
 	cout << "Sample barcode conversion" << endl;
 	for (const auto &myPair : samples_barcode_hash) {
