dc446258eb0236f007b84bcb6c6938836866d854
Rory Coffey <coffeyrt@gmail.com>
2021-11-04 21:51:12 -0400

    documentation added

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,9 +5,13 @@ using namespace std;
 string SequenceParser::fix_sequence(string& query_sequence,
 				    stringset& subject_sequences,
 				    size_t max_errors) {
+	// Allow for a better match at max errors
 	size_t best_mismatches = max_errors + 1;
-	string none = "None";
+	string none = "None";  // Create a None string to point to
+	// Creating a pointer for best match so that a string isn't continually
+	// reallocated
 	const string* best_match = &none;
+	// Initiate mismatches so it isn't continually initiated
 	unsigned int mismatches;
 	for (auto& subject_sequence : subject_sequences) {
 		if (query_sequence.size() < subject_sequence.size()) {
@@ -18,19 +22,27 @@ string SequenceParser::fix_sequence(string& query_sequence,
 			exit(1);
 		}
 		mismatches = 0;
+		// Iterate through characters from the query sequence and the
+		// subject sequence to get the number of mismatches
 		for (size_t j = 0; j < query_sequence.size(); ++j) {
 			if ((query_sequence[j] != 'N' &&
 			     subject_sequence[j] != 'N') &&
 			    query_sequence[j] != subject_sequence[j]) {
 				++mismatches;
 			}
+			// If mismatches are more than the best so far, break
+			// early
 			if (mismatches > best_mismatches) {
 				break;
 			}
 		}
+		// If two sequences are the best match, then there are no best
+		// matches so far
 		if (mismatches == best_mismatches) {
 			best_match = &none;
 		}
+		// If there are fewer mismatches, a pointer to the current
+		// subject sequence is set as best match
 		if (mismatches < best_mismatches) {
 			best_mismatches = mismatches;
 			best_match = &subject_sequence;
@@ -41,14 +53,24 @@ string SequenceParser::fix_sequence(string& query_sequence,
 
 void SequenceParser::fix_constant() {
 	stringset subject_sequences;
+	// Get the length differene between the sequencing read and the sequence
+	// format in order to stop iterations before running out of the sequence
+	// read
 	size_t length_diff = sequence.size() - sequence_format.length;
+	// Create stringset with every sequence read offset by one with the
+	// length of the sequence format.  This is to have all possible subject
+	// sequences to query from the seqeunce read
 	for (size_t i = 0; i < length_diff; ++i) {
 		subject_sequences.insert(
 		    sequence.substr(i, sequence_format.length));
 	}
+	// Find the best match between the constant region (format_string) and
+	// all possible sequences in the sequencing read
 	string best_match =
 	    fix_sequence(sequence_format.format_string, subject_sequences,
 			 sequence_format.constant_size / 5);
+	// If a match is found, proceed and replace the N's in the format string
+	// with the barcodes from the sequencing read
 	if (best_match != "None") {
 		string fixed_sequence;
 		for (size_t i = 0; i < best_match.size(); ++i) {
